c1f970e231f638b3d9efd79dab067f0d452487a3
Skrukow245 <skrukow245@gmail.com>
2021-09-16 19:28:12 -0600

    Add project files.

---
+++ Daily Report Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+        {
+        Console.WriteLine("The Tech Academy.");
+        Console.WriteLine("Student Daily Report.");
+        Console.WriteLine("What is your name?");
+        Console.WriteLine("What coruse are you on?");    //all questions
+        Console.WriteLine("What page number?");
+        Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+        Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+        Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+        Console.WriteLine("How many hours did you study today?");
+        Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day! This is the end of the program.");
+        Console.ReadLine(); //keep window up after displaying data
+
+    }
+}
