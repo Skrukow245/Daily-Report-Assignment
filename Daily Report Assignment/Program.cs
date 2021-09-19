03175e20b15f565b08f2554c33daef47e4dd55b5
Skrukow245 <skrukow245@gmail.com>
2021-09-18 19:46:47 -0600

    fixed no var

--- Daily Report Assignment/Program.cs
+++ Daily Report Assignment/Program.cs
@@ -8,12 +8,20 @@ class Program
         Console.WriteLine("The Tech Academy.");
         Console.WriteLine("Student Daily Report.");
         Console.WriteLine("What is your name?");
-        Console.WriteLine("What coruse are you on?");    //all questions
+        string yourName = Console.ReadLine();      
+        //Console.WriteLine("Your name is: " + yourName);  //wasn't sure if we needed it to read back our answers.
+        Console.WriteLine("What course are you on?");
+        string yourCourse = Console.ReadLine(); 
         Console.WriteLine("What page number?");
+        string yourPage = Console.ReadLine();
         Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+        string yourhelp = Console.ReadLine();
         Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+        string yourExperiences = Console.ReadLine();
         Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+        string yourFeedback = Console.ReadLine();
         Console.WriteLine("How many hours did you study today?");
+        string yourHours = Console.ReadLine();
         Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day! This is the end of the program.");
         Console.ReadLine(); //keep window up after displaying data
 
